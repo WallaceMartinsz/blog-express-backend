25b0a8bdd857ce30c61fc2774a5fa6b526abca99
Wallace Martins <martinsswallace@gmail.com>
2023-11-28 13:30:26 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const connectDb = require('./db/dbConnection.js');
 const postController = require('./controller/postController.js');
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 connectDb();
 
@@ -13,6 +22,5 @@ app.get('/api/posts/:id', postController.getPostById);
 app.post('/api/posts', postController.createPost);
 app.put('/api/posts/:id', postController.updatePost);
 app.delete('/api/posts/:id', postController.deletePost);
-
 
 module.exports = app;
